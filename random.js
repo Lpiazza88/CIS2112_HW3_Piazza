e50931607a836a4538d22ace1781a8ec3a2dc31f
Lpiazza88 <102708835+Lpiazza88@users.noreply.github.com>
2022-10-30 22:19:43 -0400

    asdf
    asdfasdf

--- random.js
+++ random.js
@@ -3,24 +3,38 @@ function clearTextbox()
   document.getElementById('mytextarea').value = "";
 }
 
+function loads(){
+  if(sessionStorage.getItem("Brainstew") == null)
+  {
+    journals = [
+      {post:"this is driving me insane.", title:"how is this going?", catagory: "Complaints", date: "01/01/01"},
+      {post:"this is STILL driving me insane.", title:"how is this going now?", catagory: "Complaints", date: "02/01/01"},
+      {post:"There's no hope for me.", title:"Any better?", catagory: "Complaints", date: "03/01/01"}
+    ]
+  }
+  else
+  {
+    var journals = JSON.parse(sessionStorage.getItem("Brainstew"));
+  }
+}
 function addPost()
 {
   /*alert("You hit it");*/
-  
-  var journals = JSON.parse(sessionStorage.getItem("Brainstew"));
   var post=tinymce.get('mytextarea').getContent;
   var title = document.getElementById("title").value;
   var category = document.getElementById("category").value;
   var date = new Date();
   
+  journals[journals.length]={};
+
   journals [journals.length-1].title=title;
   journals [journals.length-1].post=post;
   journals [journals.length-1].category=category;
   journals [journals.length-1].date=date;
 
   sessionStorage.setItem("Brainstew", JSON.stringify(journals));
-
-  /*var table = document.getElementById("Entries");
+/*
+  var table = document.getElementById("Entries");
   var numRows = table.rows.length;
   var numCols = table.rows[0].cells.length - 1;
   var row;
